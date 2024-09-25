0bb4ecfad1a5deb426c80f04f85100a769d31d58
yassirglya <yassir.glya@galileiostiglia.edu.it>
2024-09-25 06:52:58 +0000

    esercizio di prova

---
+++ informatica/250924.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main(){
+	float base,altezza,area;
+	
+	base=0;
+	altezza=0;
+	area=0;
+	
+	printf("inserisci la base: ");
+	scanf("%f",&base);
+	printf("inserisci l altezza: ");
+	scanf("%f",&altezza);
+	area=(base*altezza)/2;
+	printf("l'area vale:%.2f ",area);
+	
+}
+	
